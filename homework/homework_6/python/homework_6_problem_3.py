232f1985ccb0cd5340cca852d8a00c9dfcf3c19f
Evan Burke <ejbur@nighthawk.localdomain>
2022-11-09 23:05:02 -0500

    finished HW6 draft

--- homework/homework_6/python/homework_6_problem_3.py
+++ homework/homework_6/python/homework_6_problem_3.py
@@ -19,8 +19,9 @@ R = 287
 gamma = 1.2 # this is different!!!
 
 def mass_flow(pt=None, A_star=None, Tt=None, gamma=1.4, R=287):
-    mdot = pt*A_star / Tt**0.5 * (gamma/R * (2/gamma+1)**((gamma+1)/(gamma-1)))**0.5
+    mdot = pt*A_star / Tt**0.5 * (gamma/R * (2/(gamma+1))**((gamma+1)/(gamma-1)))**0.5
     print(f'Choked Mass Flow Rate = {mdot} kg/s')
+    return mdot
 
 def A_from_A_star(A_star=None,M=None,gamma=1.4):
     A = ((A_star**2/M**2) * (2/(gamma+1) * (1 + (gamma-1)/2 * M**2 ))**((gamma+1)/(gamma-1)))**0.5
@@ -35,7 +36,8 @@ print(pressures[0:10])
 
 def SSME_thrust(mdot=None,u_e=None,p_e=None,p_amb=None,A_e=None):
     thrust = mdot * u_e + (p_e-p_amb)*A_e
-    pass
+    return thrust
+    
 # To get mdot:
 # Need pt, A_th, Tt, gamma, R
 # pt is given, Tt unknown, M_th = 1, A_i/A_th known
@@ -45,7 +47,7 @@ def A_A_star(M=None,A_A_star=None,gamma=1.4):
     eq = ((1/M**2) * (2/(gamma+1) * (1 + (gamma-1)/2 * M**2 ))**((gamma+1)/(gamma-1)))**0.5 - A_A_star
     return eq
 
-M_i = float(fsolve(A_A_star,x0=0.01,args=(A_i/A_th,gamma)))
+M_i = float(fsolve(A_A_star,x0=0.150,args=(A_i/A_th,gamma)))
 print(f'Nozzle Inlet Mach = {M_i}')
 Tt = isen.get_total_temperature(M=M_i,T=T_i,gamma=gamma)
 print(f'Total temperature at nozzle inlet = {Tt} K')
@@ -54,3 +56,45 @@ mdot = mass_flow(pt=pt,A_star=A_th,Tt=Tt,gamma=gamma,R=R)
 
 # mdot, A_e, p_ambs known, need u_e and p_e
 # Use A/A* relationship to get exit Mach number, exit sonic velocity, exit velocity
+
+M_e = float(fsolve(A_A_star,x0=6,args=(A_e/A_th,gamma)))
+print(f'Exit Mach = {M_e}')
+p_e = isen.get_static_pressure(M=M_e,p_t=pt,gamma=gamma)
+print(f'Exit static pressure = {p_e} Pa')
+T_e = isen.get_static_temperature(M=M_e,T_t=Tt,gamma=gamma)
+print(f'Exit static temperature = {T_e} K')
+a_e = isen.get_sonic_velocity(T=T_e,gamma=gamma)
+print(f'Exit sonic velocity = {a_e} m/s')
+u_e = M_e*a_e
+print(f'Exit velocity = {u_e} m/s')
+
+thrusts = [SSME_thrust(mdot=mdot,u_e=u_e,p_e=p_e,p_amb=p_i,A_e=A_e)/1000 for p_i in pressures]
+
+fig,ax = plt.subplots()
+ax.set_ylabel('Pressure [Pa]')
+ax.set_xlabel('Altitude [m]')
+ax.set_title('Ambient Pressure vs. Altitude')
+plt.plot(altitudes,pressures)
+plt.savefig('../images/problem_3/pamb_vs_alt.png', bbox_inches='tight')
+plt.close()
+
+thrust_design = SSME_thrust(mdot=mdot,u_e=u_e,p_e=p_e,p_amb=p_e,A_e=A_e)/1000
+print(f'Design thrust = {thrust_design} kN')
+
+def pvh(h=None,p=None):
+    eq = 101325*(1-(2.25577*10**(-5)*h))**5.25588 - p
+    return eq
+
+alt_design = float(fsolve(pvh,x0=10000,args=(p_e)))
+print(f'Design altitude = {alt_design} m')
+
+fig,ax = plt.subplots()
+ax.set_ylabel('Thrust [kN]')
+ax.set_xlabel('Altitude [m]')
+ax.set_title('SSME Thrust vs. Altitude')
+#plt.plot(altitudes,thrusts)
+plt.plot(altitudes[0:12235],thrusts[0:12235],'b',linestyle='dotted',label='Overexpanded')
+plt.plot(altitudes[12237:],thrusts[12237:],color='orange',linestyle='dashed',label='Underexpanded')
+plt.plot(alt_design,thrust_design,'r*',label='Design Condition')
+plt.legend()
+plt.savefig('../images/problem_3/t_vs_alt.png', bbox_inches='tight')
